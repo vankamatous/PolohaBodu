113f6a434c5b5da383b8c60addf05a2563e17ce5
vankamatous <vankamatous@gmail.com>
2024-12-16 20:43:57 +0100

    základní if pro script

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const polohaY = document.getElementById("polohaY")
 const button = document.getElementById("button")
 const vysledek = document.getElementById("vysledek")
 
+
+
+button.onclick = function(){
+
+let x = Number(polohaX.value);
+let y = Number(polohaY.value);
+
+
+if ((x>25 || y>25) + (x<-25 || y<-25)){
+    vysledek.value = "nepatří"
+}
+    else{
+        vysledek.value = "patří"
+    }
+
+}
