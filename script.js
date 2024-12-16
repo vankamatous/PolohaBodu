2e50bbb72e8ea8efb8b8bfc9603a6ce85d25d520
vankamatous <vankamatous@gmail.com>
2024-12-16 20:19:14 +0100

    zakladni index formluar a constatnty v scriptu

---
+++ script.js
@@ -0,0 +1,5 @@
+const polohaX = document.getElementById("polohaX")
+const polohaY = document.getElementById("polohaY")
+const button = document.getElementById("button")
+const vysledek = document.getElementById("vysledek")
+
